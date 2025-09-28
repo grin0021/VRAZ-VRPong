b0273df5a467320736cd4f553d86accc47d02351
grin0021 <55245260+grin0021@users.noreply.github.com>
2025-09-28 19:50:39 -0400

    Updating Comments

--- Project Files/Source/VRPong/Private/HighScoreObject.cpp
+++ Project Files/Source/VRPong/Private/HighScoreObject.cpp
@@ -3,17 +3,17 @@
 
 #include "HighScoreObject.h"
 
-FString UHighScoreObject::GetPlayerName()
-{
-    return scoreInfo.Key;
-}
-
-int32 UHighScoreObject::GetScoreValue()
-{
-    return scoreInfo.Value;
-}
-
-int32 UHighScoreObject::GetLeaderboardRanking()
-{
-    return rank;
-}
+FString UHighScoreObject::GetPlayerName()           //
+{                                                   //
+    return scoreInfo.Key;                           //
+}                                                   //
+                                                    //
+int32 UHighScoreObject::GetScoreValue()             //
+{                                                   //      Small class that stores information to push to the leaderboard database, and/or store what is pulled
+    return scoreInfo.Value;                         //      from the database. Database is hosted on Retool and I was excited to use some Python and SQL basics
+}                                                   //      in a Retool workflow to create this system.
+                                                    //
+int32 UHighScoreObject::GetLeaderboardRanking()     //
+{                                                   //
+    return rank;                                    //
+}                                                   //
